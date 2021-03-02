e58b8aca8d8e15aff5bd9db046b7a02c22f51c47
Ivan La Rocca <ivanlarocca12@gmail.com>
2021-03-01 21:03:35 -0500

    endpoint changed

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 module.exports = {
   PORT: process.env.PORT || 8080,
-  API_ENDPOINT: "http://localhost:8000/api",
-  // API_ENDPOINT: "https://polar-mountain-66383.herokuapp.com/api",
+  // API_ENDPOINT: "http://localhost:8000/api",
+  API_ENDPOINT: "https://polar-mountain-66383.herokuapp.com/api",
 };
